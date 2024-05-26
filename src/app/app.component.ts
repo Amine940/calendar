5155b1c2f60d314f44de769b0e6bcdd7768a11bd
amine <amine@exemple.com>
2024-05-26 13:10:54 +0100

    calendar to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import {View , EventSettingsModel, Month} from '@syncfusion/ej2-angular-schedule';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<ejs-schedule height="850" width="1250"  [eventSettings]="eventObject" [selectedDate]="setDate" [currentView]="setView" ></ejs-schedule>`,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'cacal';
+  public setView: View='Month';
+  public setDate:Date= new Date (2024,1,4);
+  public eventObject:EventSettingsModel={
+  dataSource :[{
+    StartTime : new Date(2024,0,17,4,0),
+    EndTime : new Date(2024,0,17,6,0)
+ }] 
+ }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCeExwWmFZfVpgdVVMYl9bQXJPMyBoS35RckVmWXhfcHZWQ2VfV0N3');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
